2fec2d2462a92e7945ed94913df6b56b3ecc742f
Ariya Goudarzi <115103967+ariyagoudarzi@users.noreply.github.com>
2024-09-17 22:11:51 +0330

    Add files via upload
    made a new hook call useKey for listening the press key for doing something

---
+++ src/useKey.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      function callBack(e) {
+        if (e.code.toLowerCase() === key.toLowerCase()) {
+          action();
+        }
+      }
+      document.addEventListener("keydown", callBack);
+      return function () {
+        document.removeEventListener("keydown", callBack);
+      };
+    },
+    [key, action]
+  );
+}
